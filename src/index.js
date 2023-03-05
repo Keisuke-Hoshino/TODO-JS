705725e184fcb7b152aca982906e05b6c7dd6971
Keisuke-Hoshino <po258096@gmail.com>
2023-03-05 22:11:41 +0900

    追加ボタン押下処理

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import "./styles.css";
-
 const onClickAdd = () => {
-    alert()
-}
+    // テキストボックスの値を取得し、初期化する
+    const imputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
 
-document.getElementbyId("add-button").addEventlistener("click", () => onClickAdd())
+    // liを生成
+    const li = document.createElement("li");
+    li.className = "list-row";
+
+    // pタグ生成
+    const p = document.createElement("p");
+    p.innerText = imputText;
+
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+
+    // liタグの子要素に各要素を設定
+    li.appendChild(p);
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
+
+    // 未完了のリストに追加
+    document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+    .getElementById("add-button")
+    .addEventListener("click", () => onClickAdd());
